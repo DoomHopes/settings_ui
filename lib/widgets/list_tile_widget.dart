c1cff7693db9436c1f4c7f1627bdaf203814dbb0
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-10-25 14:10:53 +0300

    add list tile widget

---
+++ lib/widgets/list_tile_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListTileWidget extends StatelessWidget {
+  const ListTileWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

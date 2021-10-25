f98ca07d0b7ad2cd179f05a41544b38ee1f30c65
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-10-25 15:10:06 +0300

    some change

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:settings_ui/widgets/list_tile_widget.dart';
 import 'package:settings_ui/widgets/profile_widget.dart';
-import 'package:settings_ui/widgets/theme_mode_switch_widget.dart';
+import 'package:settings_ui/widgets/switch_widget.dart';
 import 'package:settings_ui/widgets/title_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _selected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,23 +28,33 @@ class HomePage extends StatelessWidget {
         shadowColor: Colors.transparent,
       ),
       body: Column(
-        children: const <Widget>[
-          ProfileWidget(),
-          SwitchWidget(),
-          TitleWidget(
+        children: <Widget>[
+          const ProfileWidget(),
+          SwitchWidget(
+            backgroundIconColor: Colors.grey,
+            text: 'Dark Mode',
+            icon: Icon(Icons.nightlight_outlined),
+            onChanged: (bool newValue) {
+              setState(() {
+                _selected = newValue;
+              });
+            },
+            value: _selected,
+          ),
+          const TitleWidget(
             text: 'Profile',
           ),
-          ListTileWidget(
+          const ListTileWidget(
             text: 'Edit profile',
             icon: Icon(Icons.contact_page_outlined),
             iconColor: Colors.amber,
           ),
-          ListTileWidget(
+          const ListTileWidget(
             text: 'Change password',
             icon: Icon(Icons.vpn_key_outlined),
             iconColor: Colors.blue,
           ),
-          TitleWidget(
+          const TitleWidget(
             text: 'Notifications',
           ),
         ],
---
+++ lib/widgets/switch_widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class SwitchWidget extends StatelessWidget {
+  final Color backgroundIconColor;
+  final String text;
+  final Icon icon;
+  final Function onChanged;
+  final bool value;
+
+  const SwitchWidget(
+      {Key? key,
+      required this.backgroundIconColor,
+      required this.text,
+      required this.icon,
+      required this.onChanged,
+      required this.value})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.transparent,
+      height: 55,
+      width: double.infinity,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 5,
+            left: 18,
+            child: Container(
+              height: 35,
+              width: 35,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: backgroundIconColor,
+              ),
+              child: icon,
+            ),
+          ),
+          Positioned(
+            top: 15,
+            left: 105,
+            child: Text(
+              text,
+              style: const TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 5,
+            right: 15,
+            child: Switch(
+              activeTrackColor: Colors.grey[300],
+              inactiveTrackColor: Colors.grey[300],
+              inactiveThumbColor: Colors.blue,
+              value: value,
+              onChanged: (bool value) {
+                onChanged(value);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
